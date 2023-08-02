b82b0c3cd4c9bbf906e79acb266b23852dc5f21c
relaxrazlom <relaxrazlom@mail.ru>
2023-08-02 09:48:05 +0300

    Fix backend/dockerfile

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -60,7 +60,8 @@ DATABASES = {
         'NAME': os.getenv('POSTGRES_DB', 'django'),
         'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_NAME', ''),
+        'HOST': os.getenv('DB_HOST', ''),
+        'PORT': os.getenv('DB_PORT', 5432)
     }
 }
 
