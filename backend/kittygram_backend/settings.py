c07fb53e29cc4adec498d2024b3e09f68d87d22e
relaxrazlom <relaxrazlom@mail.ru>
2023-08-02 22:01:28 +0300

    Fix all app

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -98,8 +98,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'collected_static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = '/media'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
